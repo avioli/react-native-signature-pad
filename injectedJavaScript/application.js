d8d1e3e1824b6b8192f0ead9db64da5caaa08a40
Evo Stamatov <aviolito@gmail.com>
2018-01-14 23:30:15 +1100

    indent

--- injectedJavaScript/application.js
+++ injectedJavaScript/application.js
@@ -14,49 +14,47 @@ export default function ({ penColor, backgroundColor, dataURL }) {
   }
 
   var canvasElement = document.querySelector('canvas');
-  {
-    /*We're rotating by 90% -> Flip X and Y*/
-    /*var width = bodyHeight;
-    var height = bodyWidth;*/
+  /*We're rotating by 90% -> Flip X and Y*/
+  /*var width = bodyHeight;
+  var height = bodyWidth;*/
 
-    var width = bodyWidth;
-    var height = bodyHeight;
+  var width = bodyWidth;
+  var height = bodyHeight;
 
-    var sizeSignaturePad = function () {
-      var devicePixelRatio = 1; /*window.devicePixelRatio || 1;*/
-      var canvasWidth = width * devicePixelRatio;
-      var canvasHeight = height * devicePixelRatio;
-      canvasElement.width = canvasWidth;
-      canvasElement.height = canvasHeight;
-      canvasElement.getContext('2d').scale(devicePixelRatio, devicePixelRatio);
-    };
+  var sizeSignaturePad = function () {
+    var devicePixelRatio = 1; /*window.devicePixelRatio || 1;*/
+    var canvasWidth = width * devicePixelRatio;
+    var canvasHeight = height * devicePixelRatio;
+    canvasElement.width = canvasWidth;
+    canvasElement.height = canvasHeight;
+    canvasElement.getContext('2d').scale(devicePixelRatio, devicePixelRatio);
+  };
 
-    var finishedStroke = function(base64DataUrl) {
-       executeNativeFunction('finishedStroke', {base64DataUrl: base64DataUrl});
-    };
+  var finishedStroke = function(base64DataUrl) {
+     executeNativeFunction('finishedStroke', {base64DataUrl: base64DataUrl});
+  };
 
-    var enableSignaturePadFunctionality = function () {
-      var signaturePad = new SignaturePad(canvasElement, {
-        penColor: '${penColor || 'black'}',
-        backgroundColor: '${backgroundColor || 'white'}',
-        onEnd: sendBase64DataUrl
-      });
-      /* signaturePad.translateMouseCoordinates = function (point) {
-        var translatedY = point.x;
-        var translatedX = width - point.y;
-        point.x = translatedX;
-        point.y = translatedY;
-      }; */
-      signaturePad.minWidth = 1;
-      signaturePad.maxWidth = 4;
-      if (${!!dataURL}) {
-        signaturePad.fromDataURL('${dataURL}');
-      }
-    };
+  var enableSignaturePadFunctionality = function () {
+    var signaturePad = new SignaturePad(canvasElement, {
+      penColor: '${penColor || 'black'}',
+      backgroundColor: '${backgroundColor || 'white'}',
+      onEnd: sendBase64DataUrl
+    });
+    /* signaturePad.translateMouseCoordinates = function (point) {
+      var translatedY = point.x;
+      var translatedX = width - point.y;
+      point.x = translatedX;
+      point.y = translatedY;
+    }; */
+    signaturePad.minWidth = 1;
+    signaturePad.maxWidth = 4;
+    if (${!!dataURL}) {
+      signaturePad.fromDataURL('${dataURL}');
+    }
+  };
 
-    sizeSignaturePad();
-    enableSignaturePadFunctionality();
-  }
+  sizeSignaturePad();
+  enableSignaturePadFunctionality();
 
   function sendBase64DataUrl () {
     var payload = {
